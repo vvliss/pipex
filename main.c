aeb4d441e228dbd85b7c4fa3bdced883b879f4ea
Wiktoria Lisson <wilisson@2-A-2.42heilbronn.de>
2025-10-25 19:35:51 +0200

    First commit - start of the project

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 16:12:10 by wilisson          #+#    #+#             */
-/*   Updated: 2025/10/25 19:03:57 by wilisson         ###   ########.fr       */
+/*   Updated: 2025/10/25 19:34:22 by wilisson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,5 @@ int main(int ac, char **av, char **envp)
     else
     {
         open_files(av[1], av[4], &fd_inp, &fd_out);
-        
     }
 }
